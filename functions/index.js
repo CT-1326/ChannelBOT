178b3cb06334682e3e682c8cc927c127e3ae675a
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2020-03-01 19:51:11 +0900

    firebase + express 첫 테스트

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,14 @@
 const functions = require('firebase-functions');
+const express = require('express')
+const app = express()
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+app.get('*', function (req, res) {
+    res.send('firebase + express test page')
+});
+
+exports.helloWorld = functions
+    .https
+    .onRequest(app);
