8f6b84d83ad027a1b7536bb68649c701bbf514bf
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2020-05-04 01:14:43 +0900

    학교번호 페이지 크롤링

--- functions/Crawling/number.js
+++ functions/Crawling/number.js
@@ -12,33 +12,43 @@ exports.scheduledFunction = functions
     .region('asia-northeast1')
     .https
     .onRequest(() => {
-        const getData = async () => {
-            const browser = await puppeteer.launch({
-                args: ['--no-sandbox', '--disable-setuid-sandbox']
+        try {
+            const getData = async () => {
+                const browser = await puppeteer.launch({
+                    args: ['--no-sandbox', '--disable-setuid-sandbox']
+                });
+                const page = await browser.newPage();
+                const ID = "smw7567";
+                const PW = "Apsj1860178*";
+                await page.goto('https://everytime.kr/login');
+                await page.evaluate((id, pw) => {
+                    document
+                        .querySelector('#container > form > p:nth-child(1) > input')
+                        .value = id;
+                    document
+                        .querySelector('#container > form > p:nth-child(2) > input')
+                        .value = pw;
+                }, ID, PW);
+                await page.click('#container > form > p.submit > input');
+                // await page.waitForNavigation();
+                await page.goto('https://everytime.kr/389111/v/79312283');
+                await page.waitForSelector('#container > div.wrap.articles > article > a > p');
+                const info = await page.$eval(
+                    '#container > div.wrap.articles > article > a > p',
+                    e => e.outerText
+                );
+                await browser.close();
+                return info;
+            }
+            getData().then(res => {
+                console.log(res);
+                admin
+                    .database()
+                    .ref('School_Number')
+                    .set({info: res});
+                return null;
             });
-            const page = await browser.newPage();
-            const ID = "smw7567";
-            const PW = "Apsj1860178*";
-            await page.goto('https://everytime.kr/login');
-            await page.evaluate((id, pw) => {
-                document
-                    .querySelector('#container > form > p:nth-child(1) > input')
-                    .value = id;
-                document
-                    .querySelector('#container > form > p:nth-child(2) > input')
-                    .value = pw;
-            }, ID, PW);
-            await page.click('#container > form > p.submit > input');
-            await page.goto('https://everytime.kr/389111/v/79312283');
-            await browser.close();
-            return page.url();
+        } catch (error) {
+            console.log('wtf : ', error);
         }
-        getData().then(html => {
-            console.log(html);
-            admin
-                .database()
-                .ref('School_Number/')
-                .set({info: html})
-            return null;
-        });
     });
