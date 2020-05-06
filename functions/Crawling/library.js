e0ec4e010e879ca02def1b1e58e847b7fb4830b8
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2020-05-06 21:47:12 +0900

    학교 도서관 좌석 크롤링 3분 간격 설정
    충돌없는 통신을 위해

--- functions/Crawling/library.js
+++ functions/Crawling/library.js
@@ -73,7 +73,7 @@ exports.library = functions
 exports.scheduledFunction = functions
     .region('asia-northeast1')
     .pubsub
-    .schedule('*/1 * * * 1-5')
+    .schedule('*/3 * * * 1-5')
     .timeZone('Asia/Seoul')
     .onRun(async () => {
         try {
