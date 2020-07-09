fd927577edf334f2042e4ee5a4acfe7ff259c4a1
BSAA7567 <33346331+BSAA7567@users.noreply.github.com>
2020-07-09 15:36:48 +0900

    학식, 공지사항 블록 연동을 위한 소스 생성
    카카오 서비스 문제로 현재 퀵 버튼은 블록 연동밖에 안됨

---
+++ functions/Router/Notice/school_Notice2.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const admin = require('firebase-admin');
+const router = express.Router();
+
+router.post('/', async function (req, res) {
+    const responseBody = {
+        version: "2.0",
+        template: {
+            outputs: [
+                {
+                    simpleText: {
+                        text: "새소식 메뉴"
+                    }
+                }
+            ]
+        }
+    };
+
+    res
+        .status(200)
+        .send(responseBody);
+});
+
+module.exports = router;
