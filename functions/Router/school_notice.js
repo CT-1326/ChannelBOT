51059da5cec330c7f030dfc672fec8a4aa284b9a
BSAA7567 <33346331+BSAA7567@users.noreply.github.com>
2020-07-09 16:44:03 +0900

    이전 버젼으로 롤백
    노가다방식은 옳지 않음

---
+++ functions/Router/school_notice.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const admin = require('firebase-admin');
+const router = express.Router();
+
+router.post('/', async function (req, res) {
+    const responseBody = {
+        version: "2.0",
+        template: {
+            outputs: [
+                {
+                    simpleText: {
+                        text: "원하시는 학교 공지사항 메뉴를 선택해주세요!"
+                    }
+                }
+            ],
+            quickReplies: [
+                {
+                    "messageText": "학사 관련 알려줘",
+                    "action": "block",
+                    "blockId": "5e1d8279ffa748000195d739",
+                    "label": "학사"
+                }, {
+                    "messageText": "새소식 관련 알려줘",
+                    "action": "message",
+                    "label": "새소식"
+                }, {
+                    "messageText": "장학/등록 관련 알려줘",
+                    "action": "message",
+                    "label": "장학/등록"
+                }, {
+                    "messageText": "입학 관련 알려줘",
+                    "action": "message",
+                    "label": "입학"
+                }, {
+                    "messageText": "취업 관련 알려줘",
+                    "action": "message",
+                    "label": "취업"
+                }, {
+                    "messageText": "행사 관련 알려줘",
+                    "action": "message",
+                    "label": "행사"
+                }, {
+                    "messageText": "글로벌 관련 알려줘",
+                    "action": "message",
+                    "label": "글로벌"
+                }, {
+                    "messageText": "일반 관련 알려줘",
+                    "action": "message",
+                    "label": "일반"
+                }
+            ]
+        }
+    };
+
+    res
+        .status(200)
+        .send(responseBody);
+});
+
+module.exports = router;
