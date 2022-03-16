47bcf0efc3b7add11e2bc862316fb434e9b9fd4d
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2022-03-16 16:18:30 +0900

    Update notcold.js

--- functions/Scheduler/notcold.js
+++ functions/Scheduler/notcold.js
@@ -6,66 +6,56 @@ exports.notcold = functions
     .pubsub
     .schedule('*/5 * * * *')
     .timeZone('Asia/Seoul')
-    .onRun(() => {
-        const cold_normal = async () => {
-            const userRequest = {
-                user: {
-                    "properties": {
-                        "isFriend": false
-                    }
+    .onRun(async () => {
+        const userRequest = {
+            user: {
+                "properties": {
+                    "isFriend": false
                 }
-            };
-            return await axios.post(
+            }
+        };
+        const body = {
+            userRequest: {
+                "utterance": "면 종류 메뉴 알려줘"
+            }
+        };
+        
+        await axios
+            .post(
                 'https://asia-northeast1-channelbot-d349b.cloudfunctions.net/middleWare/schoolB' +
                         'us',
-                userRequest
-            );
-        };
-        const colde_cafe = async () => {
-            const body = {
-                userRequest: {
-                    "utterance": "면 종류 메뉴 알려줘"
-                }
-            };
-            return await axios.post(
+                {userRequest}
+            )
+            .then(result => {
+                console.log(result.status);
+            })
+            .catch(e => {
+                console.error('Errof from notcold bus : ', e);
+            });
+        await axios
+            .post(
                 'https://asia-northeast1-channelbot-d349b.cloudfunctions.net/middleWare/schoolC' +
                         'afe/schoolCafe_service',
-                body
+                {body}
             )
-        };
-        const cold_notice = async () => {
-            const body = {
-                userRequest: {
-                    "utterance": "학사 관련해서 알려줘"
-                }
-            };
-            return await axios.post(
+            .then(result => {
+                console.log(result.status);
+            })
+            .catch(e => {
+                console.error('Errof from notcold cafeService : ', e);
+            });
+        await axios
+            .post(
                 'https://asia-northeast1-channelbot-d349b.cloudfunctions.net/middleWare/schoolN' +
                         'otice/schoolNotice_service',
-                body
+                {body}
             )
-        };
+            .then(result => {
+                console.log(result.status);
+            })
+            .catch(e => {
+                console.error('Errof from notcold noticeService : ', e);
+            });
 
-        cold_normal()
-            .then(result => {
-                console.log('cold_normal:', result.status);
-            })
-            .catch(e => {
-                console.error('cold_normal break was fail...:', e);
-            });
-        colde_cafe()
-            .then(result => {
-                console.log('cold_cafe:', result.status);
-            })
-            .catch(e => {
-                console.error('cold_cafe break was fail...:', e);
-            });
-        cold_notice()
-            .then(result => {
-                console.log('cold_notice:', result.status);
-            })
-            .catch(e => {
-                console.error('cold_notice break was fail...:', e);
-            });
         return null;
     });
