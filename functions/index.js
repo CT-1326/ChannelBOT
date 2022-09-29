4854aff914c5b387f1937db908f0bbe95a14ef34
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2022-09-29 22:36:03 +0900

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -27,15 +27,15 @@ const notcold = require('./notcold');
 app.use(express.json()); // 모든 입력&출력은 JSON 포맷으로
 app.use(cors()); // cors 설정
 /* 서비스 선택 관련 미들웨어/컨트롤러 */
-app.use('/school-bus', schoolBus); 
-app.use('/school-number', schoolNumber); 
-app.use('/school-library', schoolLibrary); 
-app.use('/school-cafe', schoolCafe); 
-app.use('/school-cafe/service', cafeService); 
-app.use('/school-notice', schoolNotice); 
-app.use('/school-notice/service', noticeService); 
-app.use('/school-weather', schoolWeather); 
-app.use('/school-wifi', schoolWifi); 
+app.use('/school-bus', schoolBus);
+app.use('/school-number', schoolNumber);
+app.use('/school-library', schoolLibrary);
+app.use('/school-cafe', schoolCafe);
+app.use('/school-cafe/service', cafeService);
+app.use('/school-notice', schoolNotice);
+app.use('/school-notice/service', noticeService);
+app.use('/school-weather', schoolWeather);
+app.use('/school-wifi', schoolWifi);
 app.use('/school-wifi/service', wifiService);
 exports.middleWare = functions
     .region('asia-northeast1')
@@ -43,12 +43,16 @@ exports.middleWare = functions
     .onRequest(app);
 
 /* 크롤링 미들웨어 */
-exports.bus = bus.bus; 
-exports.number = number.number; 
+exports.bus = bus.bus;
+exports.number = number.number;
 exports.library = library.library;
 exports.cafe = cafe.cafe;
 exports.notice = notice.notice;
-exports.weather = weather.weather; 
+exports.weather = weather.weather;
 exports.notcold = notcold.notcold; // Cold Start 이슈 해결을 위한 모듈
 
-process.env.NODE_ENV = ( process.env.NODE_ENV && ( process.env.NODE_ENV ).trim().toLowerCase() == 'production' ) ? 'production' : 'development'; // 개발/배포 모드 구분을 위한 환경변수 지정
+process.env.NODE_ENV = (
+    process.env.NODE_ENV && (process.env.NODE_ENV).trim().toLowerCase() == 'production'
+)
+    ? 'production'
+    : 'development'; // 개발/배포 모드 구분을 위한 환경변수 지정
